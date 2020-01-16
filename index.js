0c97cb8cf4c78e2641cc27caab08bc3f05803389
Maciej Sznurawa <msznurawa@gmail.com>
2020-01-16 10:27:35 +0100

    Add project files

---
+++ index.js
@@ -0,0 +1,31 @@
+const formatDate = (timeInSeconds) => {
+  const hours = Math.floor(timeInSeconds/3600);
+  const minutes = Math.floor(timeInSeconds/60) - (hours * 60);
+  const seconds = timeInSeconds % 60;
+  if (timeInSeconds === 0 || (timeInSeconds > 0 && timeInSeconds < 60)) {
+    return `${timeInSeconds}s`;
+  }
+  else if (timeInSeconds === NaN || timeInSeconds === undefined || timeInSeconds === null) {
+    return `0s`;
+  }
+  else if (timeInSeconds > 59 && timeInSeconds < 3600 && seconds !== 0) {
+    return `${minutes}m ${seconds}s`;
+  }
+  else if (timeInSeconds > 59 && timeInSeconds < 3600 && seconds === 0) {
+    return `${minutes}m`;
+  }
+  else if (hours !== 0 && minutes !== 0 && seconds !== 0) {
+    return `${hours}h ${minutes}m ${seconds}s`;
+  }
+  else if (timeInSeconds % hours !== 0 && minutes === 0) {
+    return `${hours}h ${seconds}s`;
+  }
+  else if (timeInSeconds % hours !== 0 && seconds === 0) {
+    return `${hours}h ${minutes}m`;
+  }
+  else if (timeInSeconds % hours === 0) {
+    return `${hours}h`;
+  }
+}
+
+module.exports = formatDate;
